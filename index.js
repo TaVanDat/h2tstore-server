206d8a090fc268e3f189d28044fe5a407ddda92b
your name <vandat.bk52@gmail.com>
2022-04-15 18:04:46 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 // const sql = require('mssql')
 
 app.use(bodyParser.urlencoded({ extended: false }));
