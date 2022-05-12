d77c455442c4d03de69ba72413423d65bbcc41ed
your name <vandat.bk52@gmail.com>
2022-05-13 04:16:54 +0700

    fix: two function a name

--- app/Models/bill.model.js
+++ app/Models/bill.model.js
@@ -3,16 +3,15 @@ const sql = require('mssql')
 
 module.exports = function () {
     this.getOrder = async function (result) {
-        const sqlString = 'SELECT od.Id,od.Code,od.Total,od.StatusId,od.TransformMethod,ac.Name as Account ' +
-            'FROM OrderProduct od JOIN Account ac ON ac.Id = od.AccountId'
+        const sqlString = 'SELECT od.Id, od.Code, od.Total, od.StatusId, od.TransformMethod, ac.Name as Account FROM OrderProduct od JOIN Account ac ON ac.Id = od.AccountId'
         const pool = await conn;
         return pool.request()
             .query(sqlString, function (err, response) {
-                if (response.recordset.length > 0) { result(false, response.recordset); }
+                if (response?.recordset.length > 0) { result(false, response.recordset); }
                 else { result(true, null); }
             })
     }
-    this.getOrder = async function (result) {
+    this.getOrderDetail = async function (result) {
         const sqlString = 'select * from OrderProductContent where '
         const pool = await conn;
         return pool.request()
